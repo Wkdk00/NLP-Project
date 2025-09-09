c7b8a2736d4f51840fd850190e72f9d15655add1
Wkdk00 <127743024+Wkdk00@users.noreply.github.com>
2025-09-09 15:09:45 +0300

    diarization.py

--- src/model/diarization.py
+++ src/model/diarization.py
@@ -1,6 +1,5 @@
 from pyannote.audio import Pipeline
 
-"""Загрузка модели диаризации"""
 
 def load_diarization_model(hf_token):
     print("🔄 Загрузка модели диаризации...")
@@ -12,12 +11,12 @@ def load_diarization_model(hf_token):
         # Критические параметры:
         pipeline.instantiate({
             "clustering": {
-                "method": "average",  # Более стабильный алгоритм
-                "threshold": 0.7,     # Повышаем порог сходства
-                "min_cluster_size": 3 # Минимальное количество сегментов для спикера
+                "method": "average",
+                "threshold": 0.7,
+                "min_cluster_size": 3
             },
             "segmentation": {
-                "min_duration_off": 0.5  # Минимальная пауза между репликами
+                "min_duration_off": 0.5
             }
         })
         return pipeline
@@ -25,7 +24,6 @@ def load_diarization_model(hf_token):
         print(f"❌ Ошибка загрузки модели диаризации: {str(e)}")
         return None
 
-"""Определение спикера для временного интервала"""
 def find_speaker(diarization, start_time, end_time):
     for turn, _, speaker in diarization.itertracks(yield_label=True):
         if turn.start <= start_time <= turn.end or turn.start <= end_time <= turn.end:
